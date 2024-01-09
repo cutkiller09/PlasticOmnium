1d070d6c67c60755f4844254255cea181d395fb5
CORP\seloisel <sebastien.loisel@capgemini.com>
2024-01-09 13:43:23 +0100

    MAj TEst template

--- Scripts_LTS/01_Script/Templates/Test_Body_Template.c
+++ Scripts_LTS/01_Script/Templates/Test_Body_Template.c
@@ -1,14 +1,14 @@
         /*************************************************************************************/
         /* CASE $UNIT_$FUNCTION :  Nominal test case	                                     */
         Prv_Init_Variables();								     
-        t_main_Status=$UNIT_$FUNCTION($PARAMETER);
+        t_main_Status=$UNIT_$FUNCTION();
         MACRO_ASSERT_TEST( u16_i , t_main_Status, E_OK ) ;  
         u16_i++;
 
         /*************************************************************************************/
         /* CASE $UNIT_$FUNCTION :  Robustness test case					     */
         Prv_Init_Variables();		
-        t_main_Status=$UNIT_$FUNCTION($PARAMETER));
+        t_main_Status=$UNIT_$FUNCTION();
         MACRO_ASSERT_TEST( u16_i , t_main_Status, E_NOT_OK ) ;  
         u16_i++;
   
